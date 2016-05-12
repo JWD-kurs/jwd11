94378dbbcca623ecde1faba95b68e862e9f92fa9
Zeljko Vukovic <zeljko.vukovic.ns@gmail.com>
2016-05-12 09:36:40 +0200

    lab5-begin

---
+++ lab5-begin/src/main/java/jwd/wafepa/web/controller/ApiAddressController.java
@@ -0,0 +1,76 @@
+package jwd.wafepa.web.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jwd.wafepa.model.Address;
+import jwd.wafepa.service.AddressService;
+
+@Controller
+@RequestMapping("/api/address")
+public class ApiAddressController {
+	@Autowired
+	private AddressService addressService;
+
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<Address>> get(){
+		List<Address> addresses = addressService.findAll();
+		
+		return new ResponseEntity<List<Address>>(
+				addresses, 
+				HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Address> get(@PathVariable Long id){
+		Address address = addressService.findOne(id);
+		
+		return new ResponseEntity<Address>(
+				address, 
+				HttpStatus.OK);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Address> add(
+			@RequestBody Address newAddress){
+		
+		Address persisted = addressService.save(newAddress);
+		
+		return new ResponseEntity<>(
+				persisted, 
+				HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.PUT)
+	public ResponseEntity<Address> edit(
+		@PathVariable Long id,
+		@RequestBody Address editedAddress){
+	
+		if(id==null || !id.equals(editedAddress.getId())){
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		
+		Address persisted = addressService.save(editedAddress);
+		
+		return new ResponseEntity<>(
+				persisted,
+				HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.DELETE)
+	public ResponseEntity<Address> delete(
+			@PathVariable Long id){
+		
+		addressService.delete(id);
+		
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+}
