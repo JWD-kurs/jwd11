a84c62986ea416886e8027834a8bfb34b31af3d3
Marko <jocicmarko@gmail.com>
2016-05-14 17:30:32 +0200

    lab6-end

---
+++ lab6-end/src/main/webapp/static/app/js/main.js
@@ -0,0 +1,14 @@
+var wafepaApp = angular.module('wafepaApp', ['ngRoute']);
+
+wafepaApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl : '/static/app/html/partial/home.html'
+        })
+        .when('/activities', {
+            templateUrl : '/static/app/html/partial/activities.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
